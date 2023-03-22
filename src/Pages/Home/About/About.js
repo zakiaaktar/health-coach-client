0d3210f4b04f31644f497bf5c8344306dac7a3a2
Zakia Aktar <zakiaaktar@Zakias-MacBook-Pro.local>
2023-03-22 13:25:53 +0100

    add about section

--- src/Pages/Home/About/About.js
+++ src/Pages/Home/About/About.js
@@ -1,10 +1,30 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import about from '../../../assets/images/about.jpg';
+//import about1 from '../../../assets/images/about1.png'
+
 
 const About = () => {
     return (
-        <div>
-            
+        <div className="hero my-20 font-serif">
+        <div className="hero-content justify-between flex-col sm:w-full lg:flex-row">
+            <div className='relative sm:w-1/2 h-full'>
+                <img src={about} alt="" className=" h-full" />
+                {/* <img src={about1} alt="" className="absolute w-3/5 right-5 top-1/2 border-8 rounded-lg shadow-2xl" /> */}
+                
+            </div>
+            <div className='w-1/2'>
+                <p className="text-4xl font-bold text-green-500">About Me</p>
+                <h1 className="my-5">
+                As a health coach, my mission is to empower individuals to take charge of their health and well-being by providing personalized guidance and support.  <br />
+                    & of experience <br />
+                    in this field.</h1>
+                <p>My background includes a degree in nutrition and dietetics, as well as certification as a health coach. I stay up-to-date with the latest research and trends in the health and wellness field, and I am committed to providing evidence-based recommendations that are tailored to each individual's needs and preferences.</p>
+                
+                <Link to='/' className="btn bg-green-500 rounded-full px-6 py-3 border-none mt-7 font-bold">Get more info</Link>
+            </div>
         </div>
+    </div>
     );
 };
 
--- src/Pages/Home/Services/Services.js
+++ src/Pages/Home/Services/Services.js
@@ -1,9 +1,37 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import ServiceCard from './ServiceCard';
 
 const Services = () => {
+    const [services, setServices] = useState([]);
+
+
+
+    useEffect(() => {
+        fetch('https://health-coach-server-blue.vercel.app/services')
+            .then(res => res.json())
+            .then(data => setServices(data))
+    }, []);
+
+
+
+
     return (
-        <div>
-            
+        <div className='my-20'>
+            <div className='text-center'>
+                <h2 className="text-4xl font-serif font-bold text-green-500"> Service Area</h2>
+                <div className='w-1/2 mx-auto my-8'>
+                    <p>Health coaches are trained professionals who work with clients to develop personalized health plans and provide guidance and support as they work towards their health and wellness goals. </p>
+                </div>
+            </div>
+
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3'>
+                {
+                    services.map(service => <ServiceCard
+                        key={service._id}
+                        service={service}
+                    ></ServiceCard>)
+                }
+            </div>
         </div>
     );
 };
